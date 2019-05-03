b18ff6673119c78aeba0b97eea9d827df3cd100d
Mateusz Czapliński <czapkofan@gmail.com>
2019-05-03 21:53:04 +0200

    initial JNI library

--- hello-jni.c
+++ hello-jni.c
@@ -1,13 +1,20 @@
 /*
  Tried to compile with:
 
-c:> \android-ndk-r19c\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi16-clang -pie hello-jni.c
+c:> \android-ndk-r19c\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi16-clang -fPIC -c hello-jni.c
+c:> \android-ndk-r19c\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi16-clang -shared hello-jni.o -o hello-jni.so
 
- based on: http://nickdesaulniers.github.io/blog/2016/07/01/android-cli/
+ based on: https://github.com/skanti/Android-Manual-Build-Command-Line/blob/3fea20b3b52ac04cb0208a691529a89cfb2064c1/hello-jni/Makefile
  However, apparently you can't run such a binary on a non-rooted phone.
 
+https://github.com/googlesamples/android-ndk/blob/86ceeb248bb20bfffb09330d1a95bcf3e91bb99d/hello-jni/app/src/main/cpp/hello-jni.c
 */
-#include <stdio.h>
-int main() {
-	puts("Hello mello\n");
+
+#include <string.h>
+#include <jni.h>
+
+JNIEXPORT jstring JNICALL
+Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env, jobject thiz )
+{
+	return (*env)->NewStringUTF(env, "Hello from JNI..!");
 }
