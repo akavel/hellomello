c323dbd2682ff793eec5e7dfb659f6a2529ff711
Mateusz Czapliński <czapkofan@gmail.com>
2019-10-08 02:33:43 +0200

    update to dali v0.2.1, with classes_dex macro

--- hello.nim
+++ hello.nim
@@ -1,24 +1,56 @@
-# Based on:
-# - https://forum.nim-lang.org/t/2696#16699
+# Resources:
+# - System.loadLibrary - hello-jni in Android samples, and also:
+#   https://github.com/skanti/Android-Manual-Build-Command-Line/blob/3fea20b3b52ac04cb0208a691529a89cfb2064c1/hello-jni/src/com/example/hellojni/HelloJNI.java#L25
+# - <clinit> - http://mariokmk.github.io/programming/2015/03/06/learning-android-bytecode.html
 
-# TODO: compile with -d:noSignalHandler (https://forum.nim-lang.org/t/2696#16699)
+{.experimental: "codeReordering".}
+import dali
 
-# import jnim
-# import dynlib # FIXME: Do I need this?
+when defined android:
+  import jnim
 
-import jni_wrapper
+const
+  Activity = "Landroid/app/Activity;"
+  HelloActivity = "Lcom/akavel/hello2/HelloActivity;"
+  Bundle = "Landroid/os/Bundle;"
+  TextView = "Landroid/widget/TextView;"
+  System = "Ljava/lang/System;"
+  String = "Ljava/lang/String;"
+  Context = "Landroid/content/Context;"
+  CharSequence = "Ljava/lang/CharSequence;"
+  View = "Landroid/view/View;"
 
-# Copied from jnim (which requires JVM to compile :/)
-# Specifically, from: https://github.com/yglukhov/jnim/blob/ec889fd4f58a8f587b53ee3b726de8189cc59769/src/private/jni_wrapper.nim
-# type
-#   jobject_base {.inheritable, pure.} = object
-#   jobject* = ptr jobject_base
-#   jstring* = ptr object of jobject
-#   JNIEnv* = ptr JNINativeInterface
-#   JNIEnvPtr* = ptr JNIEnv
+classes_dex:
+  dclass com.akavel.hello2.HelloActivity {.public.} of Activity:
+    proc `<clinit>`() {.static, constructor, regs:2, ins:0, outs:1.} =
+      # v0 = "hello-mello"
+      const_string(0, "hello-mello")
+      # System.loadLibrary(v0)
+      invoke_static(0, jproto System.loadLibrary(String))
+      # return
+      return_void()
+    proc `<init>`() {.public, constructor, regs:1, ins:1, outs:1.} =
+      # super.<init>()
+      invoke_direct(0, jproto Activity.`<init>`())
+      # return
+      return_void()
+    proc onCreate(Bundle) {.public, regs:4, ins:2, outs:2.} =
+      # super.onCreate(arg0)
+      invoke_super(2, 3, jproto Activity.onCreate(Bundle))
+      # v0 = new TextView(this)
+      new_instance(0, TextView)
+      invoke_direct(0, 2, jproto TextView.`<init>`(Context))
+      # v1 = this.stringFromJNI()
+      #  NOTE: failure to call a Native function should result in
+      #  java.lang.UnsatisfiedLinkError exception
+      invoke_virtual(2, jproto HelloActivity.stringFromJNI() -> String)
+      move_result_object(1)
+      # v0.setText(v1)
+      invoke_virtual(0, 1, jproto TextView.setText(CharSequence))
+      # this.setContentView(v0)
+      invoke_virtual(2, 0, jproto HelloActivity.setContentView(View))
+      # return
+      return_void()
+    proc stringFromJNI(): jstring {.public, native.} =
+      return jenv.NewStringUTF(jenv, "Hello from Nim classes_dex :D")
 
-proc Java_com_akavel_hello2_HelloActivity_stringFromJNI*(env: JNIEnvPtr, thiz: jobject): jstring {.cdecl,exportc,dynlib.} =
-  return env.NewStringUTF(env, "Hello from Nim :D")
-
-# jclass com.akavel.hello2.HelloActivity:
-#   proc stringFromJNI*(): string
--- hellomello.nimble
+++ hellomello.nimble
@@ -9,13 +9,20 @@ srcDir        = "src"
 
 # Dependencies
 
-requires "nim >= 0.19.4"
-requires "dali 0.1"
+requires "nim >= 1.0.0"
+requires "dali 0.2"
+requires "jnim 0.5.1"
 
 
 # Tasks
 
 task dex, "Assemble a classes.dex file":
   mkDir("apk")
-  exec("nim c -r classes.nim apk/classes.dex")
+  exec("nim c -r hello.nim apk/classes.dex")
 
+task so, "Compile and link an Android .so library":
+  mkDir("apk")
+  # http://akehrer.github.io/posts/connecting-nim-to-python/
+  # https://forum.nim-lang.org/t/3575
+  # https://forum.nim-lang.org/t/2696#16699
+  exec("nim c --app:lib --os:android --cpu=arm -d:noSignalHandler --hint[CC]:on --listcmd -o:apk/armeabi-v7a/libhello-mello.so hello.nim")
