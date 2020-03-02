462146e570af87ec6f8e2a244c51851fb2296255
Mateusz Czapliński <czapkofan@gmail.com>
2020-03-02 21:59:25 +0100

    works!

--- hello.nim
+++ hello.nim
@@ -1,10 +1,13 @@
 {.experimental: "codeReordering".}
 import jnim
 
+import jnim/java/lang
+
 import android/content/context
 import android/app/activity
 import android/os/bundle
 import android/view/view
+import android/util/log
 
 jclassDef java.lang.CharSequence of JVMObject
 
@@ -24,13 +27,25 @@ jexport NimActivity extends Activity:
     $this.n
 
   proc onCreate(b: Bundle) =
+    discard Log.d("hellomello", "after onCreate prologue - START")
     this.super.onCreate(b)
+    discard Log.d("hellomello", "after super.onCreate")
     this.n = 44
+    discard Log.d("hellomello", "after this.n=44")
     let v = TextView.new(this)
+    discard Log.d("hellomello", "after TextView.new(this)")
+    # NOTE: above is the last Log.d line reached before String.new was added
+
+    let s = String.new("barfoo")
+    discard Log.d("hellomello", "after String.new('barfoo')")
+
     # # v.setText(this.stringFromField())
     # v.setText(cast[CharSequence](this.stringFromField()))
-    v.setText(cast[CharSequence]("foobar"))
+    #v.setText(cast[CharSequence]("foobar"))
+    v.setText(cast[CharSequence](s))
+    discard Log.d("hellomello", "after TextView.setText(...)")
     this.setContentView(v)
+    discard Log.d("hellomello", "after this.setContentView(TextView) - END")
 
 jnimDexWrite("jnim_gen_dex.nim")
 
