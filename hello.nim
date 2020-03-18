38b2189c48f208a1a04f91b1ab5f545fb9fef609
Mateusz Czapliński <czapkofan@gmail.com>
2020-03-18 21:51:47 +0100

    initial sketch of SurfaceView approach

--- hello.nim
+++ hello.nim
@@ -38,40 +38,69 @@ type
     live: bool
     wallW2, holeH, birdR: int
     pWall, pBird: Paint
+
+    renderer: Thread
+    holder: SurfaceHolder
+
   FlappyView = ref object of View
     # TODO: can't we avoid this indirection level?
     data: FlappyViewData
 
-jexport FlappyView extends View:
+jexport FlappyView extends SurfaceView implements Runnable:
   proc new(c: Context) = super(c)  # TODO: or else?
-  proc init0() =
+
+  proc start() =
     this.setBackgroundColor(blue)
-    this.data.x = 30
-    this.data.pWall = Paint.new()
-    this.data.pWall.setColor(green)
-    this.data.pBird = Paint.new()
-    this.data.pBird.setColor(red)
-  proc init(w, h: int32) =
-    this.data.y = int(h / 2)
-    this.data.walls = @[(int(w/2), this.data.y), (int(w), int(h/3*2))]
-    this.data.live = true
-    this.data.wallW2 = int(w/5/2)
-    this.data.holeH = int(h/6)
-    this.data.birdR = int(h/20)
-  proc onDraw(c: Canvas) =
-    var
-      width = this.getWidth()
-      height = this.getHeight()
+    let
+      w = this.getWidth()
+      h = this.getHeight()
+      d = this.data
+    d.x = 30
+    d.pWall = Paint.new()
+    d.pWall.setColor(green)
+    d.pBird = Paint.new()
+    d.pBird.setColor(red)
+    d.y = int(h / 2)
+    d.walls = @[(int(w/2), this.data.y), (int(w), int(h/3*2))]
+    d.live = true
+    d.wallW2 = int(w/5/2)
+    d.holeH = int(h/6)
+    d.birdR = int(h/20)
+
+    d.renderer = Thread.new(this)
+    d.holder = this.getHolder()
+
+  proc stop() =
+    this.data.renderer.interrupt()
+
+  proc run() =
     let d = this.data
-    if d.walls.len == 0:
-      this.init(width, height)
+    while true:
+      if this.interrupted():
+        break
+      if !d.holder.getSurface().isValid():
+        Thread.sleep(1000)
+        continue
+      let c = d.holder.lockCanvas()
+      if c == nil:
+        continue
+      this.draw(c)
+      d.holder.unlockCanvasAndPost(c)
+      Thread.sleep(16)  # VERY roughly ~60fps
+
+  proc draw(c: Canvas) =
+    let d = this.data
     for w in d.walls:
       c.drawRect(w.x.float-d.wallW2.float, 0.float, w.x.float+d.wallW2.float, w.y.float-d.holeH.float, d.pWall)
       c.drawRect(w.x.float-d.wallW2.float, w.y.float+d.holeH.float, w.x.float+d.wallW2.float, height.float, d.pWall)
     c.drawCircle(width/2, d.y.float, d.birdR.float, d.pBird)
 
 
-type NimActivity = ref object of Activity
+type
+  NimActivityData = ref object
+    v: FlappyView
+  NimActivity = ref object of Activity
+    data: NimActivityData
 
 jexport NimActivity extends Activity:
   proc new() = super()  # TODO: or else?
@@ -79,10 +108,17 @@ jexport NimActivity extends Activity:
   proc onCreate(b: Bundle) =
     discard Log.d("hellomello", "NimActivity.onCreate begin")
     this.super.onCreate(b)
-    var v = FlappyView.new(this)
-    v.init0()
+    this.data.v = FlappyView.new(this)
     this.setContentView(v)
     discard Log.d("hellomello", "NimActivity.onCreate end")
+
+  proc onResume() =
+    super.onResume()
+    this.data.v.start()
+
+  proc onPause() =
+    super.onPause()
+    this.data.v.stop()
 
 when defined(jnimGenDex):
   jnimDexWrite("jnim_gen_dex.nim")
