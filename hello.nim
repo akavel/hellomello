1cd73749a2eddb60d2668d081de7224b3de4cce4
Mateusz Czapliński <czapkofan@gmail.com>
2020-04-25 23:40:23 +0200

    remove some unused code

--- hello.nim
+++ hello.nim
@@ -52,15 +52,9 @@ expandMacros: expandMacros:
 
     proc surfaceChanged*(holder: SurfaceHolder; format, width, height: jint) =
       discard Log.d("hellomello", "FlappyView.surfaceChanged")
-      # discard Log.d("hellomello", "w h = " & $(this.getWidth, this.getHeight))
       discard
     proc surfaceCreated*(holder: SurfaceHolder) =
-      # let d = this.data
-      # if d.renderer != nil:
-      #   d.renderer.join()
-      # d.renderer = Thread.new(cast[Runnable](this))
       discard Log.d("hellomello", "FlappyView.surfaceCreated")
-      # discard Log.d("hellomello", "w h = " & $(this.getWidth, this.getHeight))
       let
         w = this.getWidth()
         h = this.getHeight()
@@ -83,15 +77,7 @@ expandMacros: expandMacros:
 
     proc start() =
       discard Log.d("hellomello", "FlappyView.start begin")
-      # this.setBackgroundColor(blue)
-      # this.setWillNotDraw(false)
       let d = this.data
-      # d.x = 30
-      # d.pWall = Paint.new()
-      # d.pWall.setColor(green)
-      # d.pBird = Paint.new()
-      # d.pBird.setColor(red)
-
       # TODO: can we avoid 'super' below? getWidth() doesn't complain, why?
       d.holder = this.super.getHolder()
       # TODO: can we avoid cast below?
@@ -107,61 +93,30 @@ expandMacros: expandMacros:
 
     proc dodraw(c: Canvas) =
       # discard Log.d("hellomello", "FlappyView.dodraw begin")
-      # discard Log.d("hellomello", "canvas w/h =" & $(c.getWidth, c.getHeight))
       let d = this.data
-      # discard Log.d("hellomello", "FlappyView.dodraw after this.data")
       let height = this.getHeight()
-      # discard Log.d("hellomello", "FlappyView.dodraw after this.getHeight")
       let width = this.getWidth()
-      # discard Log.d("hellomello", "FlappyView.dodraw after this.getWidth")
-      # discard Log.d("hellomello", "width=" & $width)
       for w in d.walls:
-        # discard Log.d("hellomello", "FlappyView.dodraw after for start")
-        # discard Log.d("hellomello", "this.getWidth=" & $this.getWidth)
         c.drawRect(w.x.float-d.wallW2.float, 0.float, w.x.float+d.wallW2.float, w.y.float-d.holeH.float, d.pWall)
-        # discard Log.d("hellomello", "FlappyView.dodraw after drawRect 1")
-        # discard Log.d("hellomello", $(w.x.float-d.wallW2.float, 0.float, w.x.float+d.wallW2.float, w.y.float-d.holeH.float, d.pWall))
         c.drawRect(w.x.float-d.wallW2.float, w.y.float+d.holeH.float, w.x.float+d.wallW2.float, height.float, d.pWall)
-        # discard Log.d("hellomello", "FlappyView.dodraw after drawRect 2")
-        # discard Log.d("hellomello", $(w.x.float-d.wallW2.float, w.y.float+d.holeH.float, w.x.float+d.wallW2.float, height.float, d.pWall))
-      # discard Log.d("hellomello", "FlappyView.dodraw after for end")
       c.drawCircle(width/2, d.y.float, d.birdR.float, d.pBird)
-      # discard Log.d("hellomello", "FlappyView.dodraw after drawCircle")
       # discard Log.d("hellomello", "FlappyView.dodraw end")
 
     proc run() =
       discard Log.d("hellomello", "FlappyView.run begin")
       let d = this.data
-      # discard Log.d("hellomello", "FlappyView.run after d=this.data")
       while true:
-        # discard Log.d("hellomello", "FlappyView.run after while start")
         if Thread.interrupted().bool:
-          # discard Log.d("hellomello", "FlappyView.run after interrupted break")
           break
-        # discard Log.d("hellomello", "FlappyView.run after if interrupted")
-        # discard Log.d("hellomello", "FlappyView.run isValid? " & $this.super.getHolder().getSurface().isValid())
         if not d.holder.getSurface().isValid().bool:
-          # discard Log.d("hellomello", "FlappyView.run after not isValid")
           Thread.sleep(1000)
-          # discard Log.d("hellomello", "FlappyView.run after sleep(1000)")
           continue
-        # discard Log.d("hellomello", "FlappyView.run after if not isValid")
         let c = d.holder.lockCanvas()
-        # discard Log.d("hellomello", "FlappyView.run after lockCanvas")
         if isnil c: # NOTE: SIGSEGV when `if c == nil:`
-          # discard Log.d("hellomello", "FlappyView.run after c==nil")
           continue
-        # discard Log.d("hellomello", "FlappyView.run after if c==nil")
         this.dodraw(c)
-        # discard Log.d("hellomello", "FlappyView.run after dodraw")
-        # var p = Paint.new()
-        # p.setColor(white)
-        # c.drawLine(0, 0, 20, 20, p)
-        # c.drawLine(20, 0, 0, 20, p)
         d.holder.unlockCanvasAndPost(c)
-        # discard Log.d("hellomello", "FlappyView.run after unlockCanvasAndPost")
         Thread.sleep(16)  # VERY roughly ~60fps
-        # discard Log.d("hellomello", "FlappyView.run after sleep(16)")
       discard Log.d("hellomello", "FlappyView.run end")
 
 
@@ -182,7 +137,6 @@ jexport NimActivity extends Activity:
     v.super.getHolder().addCallback(v)
     # this.super.setContentView(this.data.v)
     this.super.setContentView(v)
-    # v.super.setVisibility(0)  # VISIBLE
     discard Log.d("hellomello", "NimActivity.onCreate end")
 
   proc onResume() =
